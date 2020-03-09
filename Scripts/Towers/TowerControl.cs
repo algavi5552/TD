2f23f93e47caafe462476c8a5f9e2d543d6a2374
algavi5552 <56834981+algavi5552@users.noreply.github.com>
2020-03-09 19:31:13 +0300

    Add files via upload
    теперь башни стреляют по врагам

--- Scripts/Towers/TowerControl.cs
+++ Scripts/Towers/TowerControl.cs
@@ -8,14 +8,12 @@ public class TowerControl : MonoBehaviour
     float timeBetweenAttacks;
     [SerializeField]
     float attackRadius;
-    
+    [SerializeField]
     Projectile projectile;
     Enemy targetEnemy = null;
+    bool isAttacking = false;
 
-    float attackCounter;
-
-
-
+    float attackCounter;//задержка между выстрелами  для нашей башни
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +24,93 @@ public class TowerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        attackCounter -= Time.deltaTime;//запускаем счет между выстрелами
+
+        if (targetEnemy == null)//если нет врага в прицеле
+        {
+            Enemy nearestEnemy = GetNearestEnemy();
+            if (nearestEnemy != null && Vector2.Distance
+                (transform.localPosition, nearestEnemy.transform.localPosition) <= attackRadius)//если рядом есть враг
+            {
+                targetEnemy = nearestEnemy;//взять врага на прицел
+            }
+        }
+        else
+        {
+            if (attackCounter<=0f)
+            {
+                isAttacking = true;//стреляем по откату
+
+                attackCounter = timeBetweenAttacks;//сброс счетчика стрельбы
+            }
+            else
+            {
+                isAttacking = false;
+            }
+            if (Vector2.Distance(transform.localPosition, targetEnemy.transform.localPosition) > attackRadius)
+            //если враг вышел за пределы стрельбы
+            {
+                targetEnemy = null;//снимаем врага с прицела
+            }
+            
+        }
         
+    }
+
+    IEnumerator MoveProjectile(Projectile projectile)
+    {
+        while(GetTargetDistance(targetEnemy)>0.20f && projectile!=null && targetEnemy!=null)
+            //условия стрельбы
+        {
+            var dir = targetEnemy.transform.localPosition - transform.localPosition;//расстояние должно уменьшаться
+            var angleDirection = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            //снаряд должен доворачивать
+            projectile.transform.rotation = Quaternion.AngleAxis(angleDirection, Vector3.forward);
+            projectile.transform.localPosition = Vector2.MoveTowards
+                (projectile.transform.localPosition, targetEnemy.transform.localPosition, 4f*Time.deltaTime);
+            //снаряд движется к цели
+            yield return null;
+        }
+        if (projectile!=null|| targetEnemy==null)//если снаряд есть,а цели нет
+        {
+            Destroy(projectile);
+        }
+    }
+
+    private float GetTargetDistance(Enemy thisEnemy)
+    {
+        if (thisEnemy == null)
+        {
+            thisEnemy = GetNearestEnemy();//если нет врага, то взять на прицел ближайшего
+            if (thisEnemy == null)
+            {
+                return 0f;
+            }
+        }
+        return Mathf.Abs(Vector2.Distance(transform.localPosition, thisEnemy.transform.localPosition));
+        //постоянно считываем расстояние до противника
+    }
+    public void FixedUpdate()
+    {
+        if (isAttacking == true)
+        {
+            Attack();
+        }
+    }
+    public void Attack()
+    {
+        isAttacking = false;//башня изначально не стреляет
+        Projectile newProjectile = Instantiate(projectile) as Projectile;//клонируем снаряд
+        newProjectile.transform.localPosition=transform.localPosition;//снаряд появляется в башне
+        if (targetEnemy == null)
+        {
+            Destroy(newProjectile);//если врагов в прицеле нет, удаляем снаряд
+        }
+        else
+        {
+            //двигаем снаряд к врагу
+            StartCoroutine(MoveProjectile(newProjectile));
+        }
     }
 
     private List<Enemy> GetEnemiesInRange()
@@ -35,7 +119,7 @@ public class TowerControl : MonoBehaviour
 
         foreach(Enemy enemy in Manager.Instance.EnemyList)//ищем врагов по списку из менеджера
         {
-        if (Vector2.Distance(transform.position, enemy.transform.position) <=attackRadius)
+        if (Vector2.Distance(transform.localPosition, enemy.transform.localPosition) <=attackRadius)
                 //если дистанция до врага меньше радиуса обстрела
             {
                 enemiesInRange.Add(enemy);//включить в список целей данного врага
@@ -50,10 +134,10 @@ public class TowerControl : MonoBehaviour
 
         foreach(Enemy enemy in GetEnemiesInRange())
         {
-            if (Vector2.Distance(transform.position, enemy.transform.position) < smallestDistance)
+            if (Vector2.Distance(transform.localPosition, enemy.transform.localPosition) < smallestDistance)
             //если это ближайший противник
             {
-                smallestDistance = Vector2.Distance(transform.position, enemy.transform.position);
+                smallestDistance = Vector2.Distance(transform.localPosition, enemy.transform.localPosition);
                 nearestEnemy = enemy;//то выбрать в цель стрельбы его
             }
         }
